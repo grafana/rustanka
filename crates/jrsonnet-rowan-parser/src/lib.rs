786cad55733d43688d4052a56b9e7fcacc5ceda9
Julien Duchesne <julienduchesne@live.com>
2026-02-10 11:21:01 -0500

    Merge upstream jrsonnet into rustanka (#46)
    * chore: update other impls for benchmarking
    
    * feat: digit separators
    
    * test: extra removeKey oop test from sjsonnet
    
    * fix: incorrect expression suffix handling in rowan parser
    
    ---------
    
    Co-authored-by: Yaroslav Bolyukin <iam@lach.pw>

--- crates/jrsonnet-rowan-parser/src/lib.rs
+++ crates/jrsonnet-rowan-parser/src/lib.rs
@@ -25,7 +25,7 @@ pub use token_set::SyntaxKindSet;
 
 use self::{
 	ast::support,
-	generated::nodes::{Expr, ExprBinary, ExprObjExtend},
+	generated::nodes::{Expr, ExprObjExtend},
 };
 
 pub fn parse(input: &str) -> (SourceFile, Vec<LocatedSyntaxError>) {
@@ -46,17 +46,6 @@ pub fn parse(input: &str) -> (SourceFile, Vec<LocatedSyntaxError>) {
 		},
 		parse.errors,
 	)
-}
-impl ExprBinary {
-	pub fn lhs_work(&self) -> Option<Expr> {
-		support::child(self.syntax())
-	}
-	pub fn rhs_work(&self) -> Option<Expr> {
-		let mut children = support::children(self.syntax());
-		// skip lhs
-		children.next()?;
-		children.next()
-	}
 }
 impl ExprObjExtend {
 	pub fn lhs_work(&self) -> Option<Expr> {
